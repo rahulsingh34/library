248001147a518bb84b24cb92bd6dd49461969d17
rahulsingh34 <rahulsingh342000@gmail.com>
2022-05-25 21:03:04 -0400

    finished javascript finished modal pop up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let myLibrary = [];
 
 const form = document.getElementById("addBook");
 const library = document.getElementById("library");
-let i = 0;
+let index = 0;
 
 form.addEventListener("submit", () => {
 	event.preventDefault();
@@ -11,8 +11,8 @@ form.addEventListener("submit", () => {
 	let pages = document.getElementById("pages").value;
 	let read = document.getElementById("read").checked;
 
-	if (read = "true") {
-		read = "Not Read"
+	if (read === true) {
+		read = "Not Read";
 	}
 	else {
 		read = "Read"
@@ -33,19 +33,24 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-	myLibrary.push({title, author, pages, read});
+	myLibrary.push({title, author, pages, read, index});
 	createCards();
 }
-
+	
 function createCards() {
-	for (i = i; i < myLibrary.length; i++) {
+	library.innerText = "";
+	index = 0;
+	for (i = 0; i < myLibrary.length; i++) {
 		let card = document.createElement("div");
 		card.className = "book";
+		card.setAttribute('data-index', index);
 		let title = document.createElement("h1");
 		let author = document.createElement("h1");
 		let pages = document.createElement("h1");
-		let read = document.createElement("button");
+		let read = document.createElement("button")
 		let remove = document.createElement("button");
+		read.className = "readOrNot";
+		remove.className = "remove";
 		title.innerText = '"' + myLibrary[i].title + '"';
 		author.innerText = myLibrary[i].author;	
 		pages.innerText = myLibrary[i].pages + " pages";
@@ -57,9 +62,52 @@ function createCards() {
 		card.appendChild(read);
 		card.appendChild(remove);
 		library.appendChild(card);
+		index++;
 	}
+
+	remove();
+	readOrNot();
 }
 
+function remove() {
+	let removeBtns = document.querySelectorAll(".remove");
+	removeBtns.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			let parent = btn.parentElement
+			let bookIndex = parseInt(parent.dataset.index);
+			myLibrary.splice(bookIndex,1);
+			createCards();
+		});
+	});
+}
 
+function readOrNot() {
+	let readBtns = document.querySelectorAll(".readOrNot");
+	readBtns.forEach((btn) => {
+		btn.addEventListener("click", () => {
+			let currentStatus = btn.innerText;
+			let parent = btn.parentElement;
+			let bookIndex = parseInt(parent.dataset.index);
 
+			if (currentStatus == "Read") {
+				myLibrary[bookIndex].read = "Not Read";
+			}
+			else if (currentStatus == "Not Read") {
+				myLibrary[bookIndex].read = "Read";	
+			}
+			createCards();
+		})
+	});
+}
 
+const openModal = document.getElementById("add-book");
+const closeModal = document.querySelector(".close")
+const modal = document.querySelector(".bg-modal")
+
+openModal.addEventListener("click", () => {
+	modal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+	modal.style.display = "none";
+});
